a7e6c59c295b66da05ff151431b66bac86641659
Siam-Howlader <siamhossain0804@gmail.com>
2025-05-17 21:16:36 +0600

    Add files via upload

---
+++ utils/constants.php
@@ -0,0 +1,13 @@
+<?php
+    define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/rainbow-tour/');
+    define('BASE_URL', '/rainbow-tour/');
+    
+    $DESTINATION_IMAGE_URL = 'images/destinations/';
+    $DESTINATION_IMAGE_UPLOAD_DIR = $_SERVER['DOCUMENT_ROOT'] . '/rainbow-tour/' . $DESTINATION_IMAGE_URL;
+
+    $PACKAGE_IMAGE_URL = 'images/packages/';
+    $PACKAGE_IMAGE_UPLOAD_DIR = $_SERVER['DOCUMENT_ROOT'] . '/rainbow-tour/' . $PACKAGE_IMAGE_URL;
+
+    $PROFILE_IMAGE_URL = 'images/users/';
+    $PROFILE_IMAGE_UPLOAD_DIR = $_SERVER['DOCUMENT_ROOT'] . '/rainbow-tour/' . $PROFILE_IMAGE_URL;
+?>
